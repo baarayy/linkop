11e67716d3083832969a9fed4e787b027b753512
baarayy <albaraa.ah26@icloud.com>
2023-12-27 18:32:14 +0200

    add click routes

--- src/routes/shortenRoute.ts
+++ src/routes/shortenRoute.ts
@@ -9,4 +9,11 @@ router.post("/", async (req, res) => {
   await linkModel.create({ full: req.body.fullUrl });
   res.redirect("/shortens");
 });
+router.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await linkModel.findOne({ short: req.params.shortUrl });
+  if (!shortUrl) return res.sendStatus(404);
+  shortUrl.clicks++;
+  shortUrl.save();
+  res.redirect(shortUrl.full);
+});
 export default router;
